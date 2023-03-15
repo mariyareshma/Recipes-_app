d83a34af550bdbb7391dc702793ffa848be03d12
Mariya Reshma <rreshma10653@gmail.com>
2023-03-15 12:42:26 +0530

    completed steps recipe ui

--- lib/view/recipe_widget/step.dart
+++ lib/view/recipe_widget/step.dart
@@ -9,31 +9,26 @@ class StepsWidget extends StatelessWidget {
     if (steps.isEmpty) {
       return Container();
     }
-    var result = ' ';
 
-    for (var step in steps) {
-      result = '$result$step, ';
-    }
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text('Steps :',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        ),
+        for (var item in steps)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Text(
-              'Steps :',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              '* $item',
+              style:
+                  const TextStyle(fontSize: 17, fontWeight: FontWeight.normal),
             ),
-          ),
-          Text(
-            result,
-            style: const TextStyle(fontSize: 17, fontWeight: FontWeight.normal),
-          ),
-        ],
-      ),
+          )
+      ],
     );
   }
 }
